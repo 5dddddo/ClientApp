75d76455ddd4068b337f80532353c05063126a95
minseunghwang <49888816+minseunghwang@users.noreply.github.com>
2019-10-04 14:35:21 +0900

    1004_MS 되나용

--- app/src/main/java/com/example/clientapp/fragments/ReservationFragment.java
+++ app/src/main/java/com/example/clientapp/fragments/ReservationFragment.java
@@ -26,7 +26,9 @@ public class ReservationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ;
         return inflater.inflate(R.layout.fragment_reservation, container, false);
+
     }
 
 }
